8b7b74e5df445d5fb4dd9e26584378c0978d8c5d
SOFTSERVE\skiri <skiri@softserveinc.com>
2023-07-13 15:04:54 +0300

    C#4 UC#2 Create list balance transactions endpoint

--- UseCase2/Program.cs
+++ UseCase2/Program.cs
@@ -1,19 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Options;using Stripe;
+using Stripe;
 using UseCase2;
 using UseCase2.Configurations;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddScoped(_ =>
-  new BalanceService(new StripeClient(builder.Configuration.GetSection(StripeOptions.Stripe)[StripeOptions.Token])));
+builder.Services.AddScoped<IStripeClient>(_ =>
+  new StripeClient(builder.Configuration.GetSection(StripeOptions.Stripe)[StripeOptions.Token]));
+
+builder.Services.AddScoped(serviceProvider =>
+  new BalanceService(serviceProvider.GetRequiredService<IStripeClient>()));
+
+builder.Services.AddScoped(serviceProvider =>
+  new BalanceTransactionService(serviceProvider.GetRequiredService<IStripeClient>()));
 
 var app = builder.Build();
 
 app.UseMiddleware<ErrorHandlingMiddleware>();
 app.MapGet("/balance",
-  async ([FromServices] BalanceService balanceService) => {
+  async ([FromServices] BalanceService balanceService) =>
+  {
     var response = await balanceService.GetAsync();
+
+    return Results.Ok(response);
+  });
+
+app.MapGet("/balance/transactions",
+  async ([FromServices] BalanceTransactionService balanceTransactionService, string? endingBefore, string? startingAfter) =>
+  {
+    var response = await balanceTransactionService.ListAsync(new BalanceTransactionListOptions
+    {
+      EndingBefore = endingBefore,
+      StartingAfter = startingAfter
+    });
 
     return Results.Ok(response);
   });
